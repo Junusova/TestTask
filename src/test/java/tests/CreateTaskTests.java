a70852c2088d0c6ae68b3ed576f58b4d5ac700a7
torolbaeva <toktobubu.orolbaeva@zensoft.io>
2019-04-30 19:46:28 +0600

    Added new Board

---
+++ src/test/java/tests/CreateTaskTests.java
@@ -0,0 +1,35 @@
+package tests;
+
+import io.qameta.allure.Story;
+import org.junit.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static pageObjects.BoardsPageObjects.*;
+import static testMethods.AssertMethods.*;
+import static testMethods.AuthorizationMethods.loginSuccessful;
+
+public class CreateTaskTests {
+
+  @Test
+  public void before() {
+    open("https://trello.com/login");
+    loginSuccessful();
+  }
+
+  @Story("Create And Assert New Boards")
+  @Test
+  public void test_01_createNewBoards() {
+    elementShouldHaveText(boards, "Boards");
+    clickOnElement(boards);
+    elementShouldHaveText(createNewBoardButton, "Create new board…");
+    clickOnElement(createNewBoardButton);
+    clickOnElement(chooseBlueColorQuadrant);
+    sendKeysToElement(fillInBoardNameInput, "My test task");
+    clickOnElement(clickCreateBoardButton);
+    elementShouldHaveText(getBoardName, "My test task");
+    elementShouldHaveText(getPersonalLink, "Personal");
+    elementShouldHaveText(getPrivateLink, "Private");
+    elementShouldHaveText(getAddListInput, "Add a list");
+    elementShouldHaveText(getInviteButton, "Invite");
+  }
+}
