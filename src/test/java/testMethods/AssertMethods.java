f3ece269c9b900e00cd7f7282c1bfae5afe2f2bb
torolbaeva <toktobubu.orolbaeva@zensoft.io>
2019-05-01 10:54:57 +0600

    Changed credentials

--- src/test/java/testMethods/AssertMethods.java
+++ src/test/java/testMethods/AssertMethods.java
@@ -1,33 +1,17 @@
 package testMethods;
 
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.enabled;
+import static com.codeborne.selenide.Condition.visible;
 import static junit.framework.TestCase.assertEquals;
 
 public class AssertMethods {
 
   @Step
-  public static void clickOnElement(SelenideElement element) { element.shouldBe(visible, enabled).click();
-  }
-
-  @Step
-  public static void clickOnElements(SelenideElement... element) {
-    for (SelenideElement selenideElement : element) {
-      clickOnElement(selenideElement);
-    }
-  }
-
-  @Step
-  public static void clickOnElementAndPressEnter(SelenideElement element) {
-    element.shouldBe(visible, enabled).pressEnter();
-  }
-
-  @Step
-  public static void clickOnElementByText(ElementsCollection elements, String text) {
-    elements.findBy(text(text)).click();
+  public static void clickOnElement(SelenideElement element) {
+    element.shouldBe(visible, enabled).click();
   }
 
   @Step
@@ -36,38 +20,7 @@ public class AssertMethods {
   }
 
   @Step
-  public static void sendKeysToElementAndPressEnter(SelenideElement element, String text) {
-    element.shouldBe(visible, enabled).setValue(text);
-
-  }
-
-  @Step
   public static void elementShouldHaveText(SelenideElement element, String text) {
     assertEquals(text, element.innerText());
-  }
-
-  @Step
-  public static void elementShouldHaveValue(SelenideElement element, String text) {
-    element.shouldHave(value(text));
-  }
-
-  @Step
-  public static void elementShouldHaveMatchText(SelenideElement element, String text) {
-    element.shouldHave(matchText(text));
-  }
-
-  @Step
-  public static void elementShouldBeVisible(SelenideElement element) {
-    element.shouldBe(visible);
-  }
-
-  @Step
-  public static void elementShouldNotBeVisible(SelenideElement element) {
-    element.shouldNotBe(visible);
-  }
-
-  @Step
-  public static void elementShouldNotBeExist(SelenideElement element) {
-    element.shouldNotBe(exist);
   }
 }
